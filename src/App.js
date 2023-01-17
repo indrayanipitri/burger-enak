af4d3d40cfa97721243f58ee6708ee8657b38f48
Pitri Indrayani <indrayanipitri@gmail.com>
2023-01-17 23:06:47 +0700

    page on process

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import LandingPage from '../src/Pages/LandingPage'
+import OnProcess from './Pages/OnProcess';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<LandingPage/>} />
+        <Route path='/' element={<OnProcess/>} />
+        <Route path='*' element={<OnProcess/>} />
       </Routes>
     </div>
   );
---
+++ src/Pages/OnProcess.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImgOnProcess from '../assets/process.jpg'
+
+export default function OnProcess() {
+  return (
+    <div className="container-onprocess">
+        <img src={ImgOnProcess} alt="onprocess" />
+        <h1>On Process</h1>
+        <a href="https://indrayani-pitri.netlify.app/">back</a>
+    </div>
+  )
+}
